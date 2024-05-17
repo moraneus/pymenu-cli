5fe95b2ced72a80d05110aef355dd99a6a16b549
moraneus <moraneus@gmail.com>
2024-05-17 22:27:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pymenu-cli',
-    version='1.0.0',
+    version='1.0.3',
     description='A Python library for creating interactive CLI menus',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Moraneus',
     author_email='moraneus@gmail.com',
+    url='https://github.com/moraneus/pymenu-cli',
     packages=find_packages(),
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'pymenu-cli = pymenu_cli:main'
+            'pymenu-cli = pymenu_cli.menu:main'
         ]
-    }
+    },
+    license='MIT',
 )
