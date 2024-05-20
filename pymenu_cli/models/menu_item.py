59d02d4cf5e9b37087b19f9ee4ff1b03af79b597
moraneus <moraneus@gmail.com>
2024-05-20 18:44:57 +0300

    Update menu_item.py

--- pymenu_cli/models/menu_item.py
+++ pymenu_cli/models/menu_item.py
@@ -1,8 +1,11 @@
+""" This module defines the MenuItem class, which represents an item in a menu. """
+
 from typing import Optional
 
 
 class MenuItem:
-    """Represents an item in a menu.
+    """
+    Represents an item in a menu.
 
     Attributes:
         __m_title (str): The title of the menu item.
@@ -16,7 +19,8 @@ class MenuItem:
             i_title: str,
             i_action: Optional[str] = None,
             i_submenu: Optional['Menu'] = None,
-            i_color: Optional[dict] = None):
+            i_color: Optional[dict] = None
+    ):
         """
         Args:
             i_title (str): The title of the menu item.
@@ -31,18 +35,40 @@ class MenuItem:
 
     @property
     def title(self) -> str:
+        """
+        Gets the title of the menu item.
+
+        Returns:
+            str: The title of the menu item.
+        """
         return self.__m_title
 
     @property
     def color(self) -> Optional[dict]:
+        """
+        Gets the color settings of the menu item.
+
+        Returns:
+            Optional[dict]: The color settings of the menu item.
+        """
         return self.__m_color
 
     @property
     def action(self) -> Optional[str]:
+        """
+        Gets the action associated with the menu item.
+
+        Returns:
+            Optional[str]: The action associated with the menu item.
+        """
         return self.__m_action
 
     @property
     def submenu(self) -> Optional['Menu']:
+        """
+        Gets the submenu associated with the menu item.
+
+        Returns:
+            Optional['Menu']: The submenu associated with the menu item.
+        """
         return self.__m_submenu
-
-
